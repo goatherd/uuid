c5030beed2759be2b82bc02faa9b6e1433fdb6d9
Maik Penz <maik@phpkuh.de>
2013-01-21 23:14:12 +0100

    mock unit test

---
+++ tests/Goatherd/Uuid/FactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Goatherd\Uuid;
+
+class FactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFactory()
+    {
+        $uuid = Factory::generate(Factory::UUID_TIME);
+        $this->assertTrue($this->isUuid($uuid));
+        $uuid = Factory::generate(Factory::UUID_NAME_MD5);
+        $this->assertTrue($this->isUuid($uuid));
+        $uuid = Factory::generate(Factory::UUID_RANDOM);
+        $this->assertTrue($this->isUuid($uuid));
+        $uuid = Factory::generate(Factory::UUID_NAME_SHA1);
+        $this->assertTrue($this->isUuid($uuid));
+    }
+
+    protected function isUuid($uuid) {
+        return $uuid != '';
+    }
+}
